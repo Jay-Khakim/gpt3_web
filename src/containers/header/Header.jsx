b8b3863e9c18bff24deb03be13645764b6b002b8
Jay-Khakim <mgmediajay@gmail.com>
2023-07-17 00:45:51 +0900

    Resposive Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,11 +19,10 @@ const Header = () => {
           <img src={people} alt="" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-
-        <div className="gpt3__header-image">
-          {/* <img src={ai} alt="ai" srcset="" /> */}
-        </div>
       </div>
+      <div className="gpt3__header-image">
+          <img src={ai} alt="ai" srcset="" />
+        </div>
 
     </div>
   )
